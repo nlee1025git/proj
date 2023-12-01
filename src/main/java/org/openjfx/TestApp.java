ac59fda10c511ab3cad935e7e5b61c0365ae0db9
nlee1025git <99083497+nlee1025git@users.noreply.github.com>
2023-12-01 12:40:09 -0500

    Add files via upload

---
+++ src/main/java/org/openjfx/TestApp.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestApp {
+    @Test
+    void test1() {
+        int x = 5;
+        assertequals(5, x);
+    }    
+}
